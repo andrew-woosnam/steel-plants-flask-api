33a3d14589dc5cecf585c0c9b6c755d3308c2d81
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-05 00:04:16 -0400

    create test table with sqlalchemy

--- app/populate_db.py
+++ app/populate_db.py
@@ -1,10 +1,35 @@
-import os
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, exc
+import psycopg2
+from helpers import get_db_connection_str
 
 
-def get_db_connection_str():
-    username = os.getenv('POSTGRES_USER')
-    password = os.getenv('POSTGRES_PASSWORD')
-    database_name = os.getenv('POSTGRES_DB')
-    host = os.getenv('POSTGRES_HOST', 'localhost')
-    port = os.getenv('POSTGRES_PORT', '5432')
-    return f'postgresql://{username}:{password}@{host}:{port}/{database_name}'
+def create_db_engine():
+    try:
+        engine = create_engine(
+            get_db_connection_str(),
+            echo=True)
+        return engine
+    except exc.SQLAlchemyError as err:
+        print(f"Error creating the database engine: {err}")
+
+
+def create_tables(engine):
+    metadata = MetaData()
+
+    test_table = Table(
+        'test_table', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('value', String(255)),
+    )
+
+    try:
+        metadata.create_all(engine)
+        print("Tables created successfully.")
+    except exc.SQLAlchemyError as err:
+        print(f"Error creating tables: {err}")
+
+
+if __name__ == "__main__":
+    engine = create_db_engine()
+    if engine:
+        create_tables(engine)
