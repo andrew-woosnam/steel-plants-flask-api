ddffea2e0fa6700c354a7236e7234c33583fc2c6
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-04 23:54:34 -0400

    move get_db_connection_db to new helpers file

---
+++ tests/test_helpers.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import patch
+from app.helpers import get_db_connection_str
+
+
+class TestHelpers(unittest.TestCase):
+    @patch('os.getenv')
+    def test_get_db_connection_str(self, mock_getenv):
+        mock_env_vars = {
+            'POSTGRES_USER': 'user',
+            'POSTGRES_PASSWORD': 'pass',
+            'POSTGRES_DB': 'db',
+            'POSTGRES_HOST': 'host',
+            'POSTGRES_PORT': '5432'
+        }
+
+        def getenv_side_effect(var_name, default=None):
+            return mock_env_vars.get(var_name, 'default_value')
+
+        mock_getenv.side_effect = getenv_side_effect
+
+        expected_str = 'postgresql://user:pass@host:5432/db'
+        self.assertEqual(get_db_connection_str(), expected_str)
+
+
+if __name__ == '__main__':
+    unittest.main()
