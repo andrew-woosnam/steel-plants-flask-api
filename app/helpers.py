f9bf7659a9375dd99714261d31b7ec5457fa3316
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-05 00:38:46 -0400

    add helper fn to interpret csv column types

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,6 @@
+import csv
 import os
+from sqlalchemy import Integer, String
 
 
 def get_db_connection_str():
@@ -8,3 +10,29 @@ def get_db_connection_str():
     host = os.getenv('POSTGRES_HOST', 'localhost')
     port = os.getenv('POSTGRES_PORT', '5432')
     return f'postgresql://{username}:{password}@{host}:{port}/{database_name}'
+
+
+def infer_column_types(csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        headers = next(reader)
+
+        column_types = {}
+        for header in headers:
+            column_types[header] = String(255)  # assume all strings
+
+        first_row = next(reader)  # sample data from 1st row
+        for i, value in enumerate(first_row):
+            try:
+                # Try to convert to integer
+                int(value)
+                column_types[headers[i]] = Integer()
+            except ValueError:
+                try:
+                    # Try to convert to float
+                    float(value)
+                    column_types[headers[i]] = Float()
+                except ValueError:
+                    # keep as String
+                    pass
+        return column_types
