9f9edc751976b34c2189600f2947b1ccbf669025
Andrew T. Woosnam <andrewt.woosnam@gmail.com>
2024-04-05 20:23:22 -0400

    return just names from '/api/countries'

--- src/api/list_countries.py
+++ src/api/list_countries.py
@@ -5,7 +5,7 @@ from models.country import Country
 def list_countries():
     try:
         countries = Country.query.all()
-        countries_list = [country.to_dict() for country in countries]
+        countries_list = [country.country for country in countries]
         return jsonify(countries_list), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
